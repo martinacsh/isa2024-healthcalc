a29cbef1cfa35dec607407852c7be0d3c7bd6fd3
Martina <martinacesalas@gmail.com>
2024-04-09 18:16:07 +0200

    cosas

--- src/main/java/healthcalc/gui/CalcVista.java
+++ src/main/java/healthcalc/gui/CalcVista.java
@@ -1,20 +1,11 @@
 package healthcalc.gui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.BorderLayout;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.BorderLayout;
 
 public class CalcVista extends JFrame {
 
