05cfca99368373143179f6369091a7561344fe35
Martina <martinacesalas@gmail.com>
2024-04-09 14:00:00 +0200

    controller

--- src/main/java/healthcalc/gui/Controller.java
+++ src/main/java/healthcalc/gui/Controller.java
@@ -1,5 +1,25 @@
 package healthcalc.gui;
 
-public class Controller {
+import healthcalc.HealthCalculator;
+
+import healthcalc.gui.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener {
+	private HealthCalculator modelo;
+	private CalcVista vista;
+	
+	public Controller(HealthCalculator modelo, CalcVista vista) {
+		this.modelo = modelo;
+		this.vista = vista;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
