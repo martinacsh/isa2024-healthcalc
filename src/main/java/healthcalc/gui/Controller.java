77253a6979f4049d297acdf5436c56dcfd69e401
Martina <martinacesalas@gmail.com>
2024-04-09 13:54:39 +0200

    main

---
+++ src/main/java/healthcalc/gui/Controller.java
@@ -0,0 +1,5 @@
+package healthcalc.gui;
+
+public class Controller {
+
+}
---
+++ src/main/java/mainApp/MainApp.java
@@ -0,0 +1,26 @@
+package mainApp;
+import java.awt.EventQueue;
+
+import healthcalc.HealthCalculator;
+
+import healthcalc.gui.*;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HealthCalculator modelo = new HealthCalculator();
+					CalcVista vista = new CalcVista();
+					Controller controlador = new Controller(modelo, vista);
+					vista.registrarControlador(controlador);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
