ac56abda2b4ce6339d914a588f316af6b5630e14
Martina <martinacesalas@gmail.com>
2024-05-09 22:42:02 +0200

    calculadora americana

--- src/main/java/healthcalc/Decorator/AmericanCalculator.java
+++ src/main/java/healthcalc/Decorator/AmericanCalculator.java
@@ -2,24 +2,25 @@ package healthcalc.Decorator;
 
 import healthcalc.Adapter.HealthHospital;
 
-public class AmericanCalculator extends CalculatorDecorator{
+public class AmericanCalculator extends CalculatorDecorator {
 
 	public AmericanCalculator(HealthHospital wrappedCalc) {
 		super(wrappedCalc);
 
 	}
 
-
 	@Override
 	public float idealWeightA(char gender, float height) throws Exception {
-		return 0;
+		float alturaMetros = (float) (height * 0.3048);
+		return wrappedCalculator.idealWeightA(gender, alturaMetros);
 	}
 
 	@Override
 	public float bmrA(char gender, int age, float height, int weight) throws Exception {
-		return 0;
-				
+		float alturaMetros = (float) (height * 0.3048);
+		int pesoLibras = (int) (weight * 453.592);
+		return wrappedCalculator.bmrA(gender, age, alturaMetros, pesoLibras);
+
 	}
-	
 
 }
--- src/main/java/healthcalc/Decorator/EuropeanCalculator.java
+++ src/main/java/healthcalc/Decorator/EuropeanCalculator.java
@@ -1,6 +1,5 @@
 package healthcalc.Decorator;
 
-import healthcalc.HealthCalc;
 import healthcalc.Adapter.HealthHospital;
 
 public class EuropeanCalculator extends CalculatorDecorator{
