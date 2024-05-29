2ec8e37ee57fd660ebbb85796673614fe61d1d79
Martina <martinacesalas@gmail.com>
2024-05-29 17:53:51 +0200

    parámetro tipo Person metodos healthcalculator e interfaz HealthCalc

--- src/main/java/healthcalc/HealthCalculator.java
+++ src/main/java/healthcalc/HealthCalculator.java
@@ -1,73 +1,78 @@
 package healthcalc;
 
+public class HealthCalculator implements HealthCalc {
 
-public class HealthCalculator implements HealthCalc{
-	
 	private static HealthCalculator calc;
-	
+
 	private HealthCalculator() {
 	}
-	
+
 	public static HealthCalculator getCalculator() {
-		if (calc == null){
+		if (calc == null) {
 			calc = new HealthCalculator();
 		}
-		
+
 		return calc;
 	}
-	
-    public float idealWeight(int height, Gender gender) throws Exception {
-        float resultado = 0;
-        
-        if(height <= 0){
-            throw new Exception("La altura proporcionada no es válida");
-        }
-        if(gender == Gender.FEMALE){
-            resultado = (height - 100) - ((height - 150) / 2.5f);
-        }
-        if(gender == Gender.MALE){
-            resultado = (height - 100) - ((height - 150) / 4f);
-        }
-        if(gender != Gender.MALE && gender != Gender.FEMALE){
-        throw new Exception("El género proporcionado no es válido");
-        }
 
-        if(resultado <= 0){
-            throw new Exception("El resultado es negativo. Pruebe con otros datos.");
-        }
-        return resultado;
-    }
+	public float idealWeight(Person person) throws Exception {
+		float height = person.height();
+		Gender gender = person.gender();
+		float resultado = 0;
 
+		if (height <= 0) {
+			throw new Exception("La altura proporcionada no es válida");
+		}
+		if (gender == Gender.FEMALE) {
+			resultado = (height - 100) - ((height - 150) / 2.5f);
+		}
+		if (gender == Gender.MALE) {
+			resultado = (height - 100) - ((height - 150) / 4f);
+		}
+		if (gender != Gender.MALE && gender != Gender.FEMALE) {
+			throw new Exception("El género proporcionado no es válido");
+		}
 
-public float basalMetabolicRate(float weight, int height, Gender gender, int age) throws Exception {
-    float resultado = 0;
-    if(age <= 0){
-        throw new Exception("La edad proporcionada no es válida");
-    }
-    if(height <= 0){
-        throw new Exception("La altura proporcionada no es válida");
-    }
-    if(weight <= 0){
-        throw new Exception("El peso proporcionado no es válido");
+		if (resultado <= 0) {
+			throw new Exception("El resultado es negativo. Pruebe con otros datos.");
+		}
+		return resultado;
+	}
 
-    }
-    if(gender != Gender.MALE && gender != Gender.FEMALE){
+	public float basalMetabolicRate(Person person) throws Exception {
 
-        throw new Exception("El género proporcionado no es válido");
-        }
+		float weight = person.weight();
+		float height = person.height();
+		Gender gender = person.gender();
+		int age = person.age();
+		float resultado = 0;
+		if (age <= 0) {
+			throw new Exception("La edad proporcionada no es válida");
+		}
+		if (height <= 0) {
+			throw new Exception("La altura proporcionada no es válida");
+		}
+		if (weight <= 0) {
+			throw new Exception("El peso proporcionado no es válido");
 
-    if(gender == Gender.MALE){
-        resultado = 10f * (weight + 6.25f) * (height - 5) * (age + 5);
-    }
-    if(gender == Gender.FEMALE){
-        resultado = 10 * weight + 6.25f * height - 5 * age - 161;
-        if(resultado <= 0){
-            throw new Exception("El resultado es negativo. Pruebe con otros datos");
+		}
+		if (gender != Gender.MALE && gender != Gender.FEMALE) {
 
-        }
+			throw new Exception("El género proporcionado no es válido");
+		}
 
-    }
+		if (gender == Gender.MALE) {
+			resultado = 10f * (weight + 6.25f) * (height - 5) * (age + 5);
+		}
+		if (gender == Gender.FEMALE) {
+			resultado = 10 * weight + 6.25f * height - 5 * age - 161;
+			if (resultado <= 0) {
+				throw new Exception("El resultado es negativo. Pruebe con otros datos");
 
-return resultado;
-}
+			}
+
+		}
+
+		return resultado;
+	}
 }
